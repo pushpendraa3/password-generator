cb81c7865c2910e5e58f97d8622c9bb18a62d1fc
Pushpendra <pushpendrasingh.91650@gmail.com>
2024-02-25 11:12:12 +0530

    add copy button

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useRef } from "react";
 import { useCallback } from "react";
 import { useState } from "react"
 
@@ -6,7 +8,17 @@ function App() {
   const [length, setLength] = useState(6);
   const [numAllowed, setNumAllowed] = useState(false);
   const [charAllowed, SetCharAllowed] = useState(false);
-  const [Password, setPassword] = useState("");
+  const [password, setPassword] = useState("");
+  const [isCopied, setIsCopied] = useState(false);
+
+  const passwordRef = useRef(null);
+  const copyButtonRef = useRef(null);
+
+  const copyPassword = useCallback(() => {
+    setIsCopied(true)
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+  }, [password, length, numAllowed, charAllowed])
 
   const generatePassword = useCallback(() => {
     let pass = "";
@@ -15,33 +27,38 @@ function App() {
     if (numAllowed) stringpool += "1234567890";
     if (charAllowed) stringpool += "!@#$%^&*()_+=-";
 
-    // for (const i in stringpool) {
-    //   let randomIndex = Math.floor(Math.random() * stringpool.length + 1);
-    //   pass += stringpool.charAt(randomIndex);
-    // }
-
     for (let i = 1; i <= length; i++) {
       let randomIndex = Math.floor(Math.random() * stringpool.length + 1);
       pass += stringpool.charAt(randomIndex);
-
     }
     setPassword(pass)
 
-  }, [length, numAllowed, charAllowed, setPassword])
+  }, [length, numAllowed, charAllowed, setPassword]);
+  
+  useEffect(() => { setIsCopied(false) }, [password])
+  
+  useEffect(() => {
+    generatePassword()
+  }, [length, numAllowed, charAllowed, generatePassword])
+  
   return (
     <>
 
-      <h1 className="bg-green-600 rounded-md text-4xl text-center p-2">Password Generator</h1>
+      <h1 className="bg-orange-700 rounded-md text-4xl text-center p-2">Password Generator</h1>
       <div className="w-full max-w-md mx-auto shadow-md rounded-md m-8 px-8 py-8 text-orange-400 bg-gray-800">
         <div className="flex shadow rounded-md overflow-hidden mb-4">
           <input 
           type="text" 
-          value={Password} 
+          ref={passwordRef}
+          value={password} 
           className="outline-none w-full py-1 px-8" 
           placeholder="Password"
           readOnly
           />
-          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">Copy</button>
+          <button 
+          ref={copyButtonRef}
+          onClick={copyPassword}
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">{isCopied ? 'Copied!' : 'Copy'}</button>
         </div>
         <div className="flex text-sm gap-x-2">
           <div className="flex items-center gap-x-1">
